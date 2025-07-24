e3d6f8b5ff0fba6ba067dcb0e0b48c97f791ce22
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-07-24 18:00:34 +0530

    fix: __dirname error for uploads folder in ESM

--- Server/index.js
+++ Server/index.js
@@ -8,6 +8,12 @@ import videoroutes from './Routes/video.js'
 import userroutes from "./Routes/User.js"
 import commentroutes from './Routes/comment.js'
 import path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 dotenv.config()
 const app = express()
