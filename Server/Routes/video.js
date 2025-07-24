0458131a6c783d452ee281c4d656428bb2d444e9
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-07-24 19:05:51 +0530

    fix: __dirname error for uploads folder in ESM

--- Server/Routes/video.js
+++ Server/Routes/video.js
@@ -14,7 +14,7 @@ const router = express.Router();
 
 router.get("/single/:id", getvideobyid);
 router.post("/uploadvideo", upload.single("file"), auth, uploadvideo);
-router.get("/all", getallvideo);
+
 router.patch("/like/:id", auth, likevideocontroller);
 router.put("/views/:id", viewVideo); 
 router.get("/get", getallvideo); // 👈 Add this line
--- client/src/action/video.js
+++ client/src/action/video.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import {
   GET_ALL_VIDEOS,
   GET_SINGLE_VIDEO,
